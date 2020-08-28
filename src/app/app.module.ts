0ef38f8bf4029bfcea56fc12d4d4060e29b0950c
mauriayllon <48989232+mauriayllon@users.noreply.github.com>
2020-08-28 00:08:44 -0400

    ng boostrap, bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {FormsModule} from "@angular/forms";
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { Home2Component } from './components/home2/home2.component';
+import { AboutComponent } from './components/about/about.component';
+import {Home1Component} from "./components/home1/home1.component";
+import {AppRouting} from "./app.routing";
+import {UserModule} from "./modules/user/user.module";
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    Home1Component,
+    Home2Component,
+    AboutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    UserModule,
+    AppRouting
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
